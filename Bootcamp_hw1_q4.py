15482ef86a7a4eb66dd665f4c054cbf2509a4323
nowhyee <hl5675@nyu.edu>
2024-10-05 16:17:10 -0400

    Add files via upload

---
+++ Bootcamp_hw1_q4.py
@@ -0,0 +1,12 @@
+#  Write a function sum_of_integers(a, b) that takes two integers as input from the user and returns their sum.
+
+def sum_of_integers(a, b):
+    sum = a + b
+    return sum
+
+#Example
+def main():
+    a = 3
+    b = 7
+    print(sum_of_integers(a, b))
+main()
