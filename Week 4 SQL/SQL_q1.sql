147e60b95733480a5c121a9f7b12a752003ba1aa
Hyewon <hl5675@nyu.edu>
2024-10-28 01:38:37 -0400

    Update SQL_q1.sql

--- Week 4 SQL/SQL_q1.sql
+++ Week 4 SQL/SQL_q1.sql
@@ -1,5 +1,7 @@
 /* Pull total number of orders that were completed on 18th March 2023 with the first name ‘John’ and last name Doe’ */
 SELECT COUNT(DISTINCT Order_id) AS total_orders
 FROM SALES
-WHERE Date = '2023-03-18' ;
+WHERE s.Date = '2023-03-18'
+  AND c.first_name = 'John'
+  AND c.last_name = 'Doe' ;
 
